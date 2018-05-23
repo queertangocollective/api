6ed787f930559d34ad44876d68067fe02784e421
Tim Evans <tim.c.evans@me.com>
2018-05-23 00:01:51 -0400

    add filter for upcoming events

--- app/resources/event_resource.rb
+++ app/resources/event_resource.rb
@@ -4,6 +4,10 @@ class EventResource < ApplicationResource
   belongs_to :venue, always_include_linkage_data: true
   has_many :ticketed_events
 
+  filter :upcoming, apply: ->(records, value, _options) {
+    records.where("starts_at >= ? or ends_at >= ?", DateTime.now, DateTime.now)
+  }
+
   before_create do
     @model.group = context[:group]
   end
