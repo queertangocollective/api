72170cce65a8578a22528a96adba3c90d64c2e8c
Tim Evans <tim.c.evans@me.com>
2018-05-27 19:10:54 -0400

    fix venue response

--- app/resources/venue_resource.rb
+++ app/resources/venue_resource.rb
@@ -1,4 +1,5 @@
 class VenueResource < ApplicationResource
   attributes :extended_address, :notes
-  has_one :location, always_include_linkage_data: true
+  belongs_to :location, always_include_linkage_data: true
+  belongs_to :event, always_include_linkage_data: true
 end
